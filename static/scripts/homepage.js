9b437561842b1eb7134f9708f3b314a8ce45b0bc
Winkel515 <winkel515@hotmail.com>
2020-02-23 17:32:35 -0500

    test

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -6,4 +6,9 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('orientationchange', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}, false);
+
+window.addEventListener("orientationchange", function() {
+	// Announce the new orientation number
+	alert(screen.orientation);
+}, false);
