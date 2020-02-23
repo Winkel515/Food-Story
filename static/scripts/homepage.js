b49d46f53c06d393e37162a07f8f8e1eece2838d
Winkel515 <winkel515@hotmail.com>
2020-02-23 16:42:07 -0500

    reverting

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,9 +1,4 @@
+ // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-$(document).on("pagecreate",function(){
-  $(window).on("orientationchange",function(){
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-});
