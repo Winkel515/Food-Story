9a57e44ff4703c852803183677fd611b56afd92b
Winkel515 <winkel515@hotmail.com>
2020-02-23 16:31:26 -0500

    test

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,10 +1,9 @@
- // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-$(document).on("pagecreate", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-})
-$(window).on('orientationchange', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+$(document).on("pagecreate",function(){
+  $(window).on("orientationchange",function(){
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 });
