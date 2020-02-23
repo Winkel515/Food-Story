41436bc1b3dc66db5c88429d1ee3889ca5e4538c
Winkel515 <winkel515@hotmail.com>
2020-02-23 17:57:54 -0500

    revert

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,9 +1,7 @@
-window.addEventListener('load', function () {
-  window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  })
-}, false);
+ // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('orientationchange', function() {
   window.addEventListener('resize', () => {
