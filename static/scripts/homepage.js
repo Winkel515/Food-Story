2824dc1f190ebef32d5fb465bbdfadce53e8c407
Winkel515 <winkel515@hotmail.com>
2020-02-23 17:41:38 -0500

    test

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -3,8 +3,9 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener("orientationchange", function() {
-	let vh = window.innerHeight * 0.01;
+window.addEventListener('orientationchange', function() {
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    alert('orientation change!')
+  })
 }, false);
